af03ef9982260ab8d754e6006dc568810c0225c7
William Fish <william.michael.fish@gmail.com>
2025-03-18 16:41:23 +0000

    BAU: Rename models

--- db/migrate/20250312091826_create_api_keys.rb
+++ db/migrate/20250312091826_create_api_keys.rb
@@ -1,6 +1,6 @@
 class CreateApiGatewayApiKeys < ActiveRecord::Migration[8.0]
   def change
-    create_table :api_gateway_api_keys, id: :uuid do |t|
+    create_table :api_keys, id: :uuid do |t|
       t.belongs_to :organisation, null: false, foreign_key: true, type: :uuid
       t.string :api_key_id, null: false
       t.string :api_gateway_id, null: false
--- spec/factories/api_keys.rb
+++ spec/factories/api_keys.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :api_gateway_api_key do
+  factory :api_key do
     organisation
     api_key_id { "MyString" }
     api_gateway_id { "MyString" }
