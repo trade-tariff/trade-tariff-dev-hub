d93743fabc8cc0bd6e77cea33bd1ef098ac7eb6d
Sam Johnson <sh.johnson@me.com>
2026-02-16 15:24:50 +0000

    Remove auth check for CSP

--- app/controllers/csp_reports_controller.rb
+++ app/controllers/csp_reports_controller.rb
@@ -1,5 +1,4 @@
 class CspReportsController < ApplicationController
-  skip_before_action :require_authentication, only: %i[create]
   def create
     Rails.logger.warn "CSP Violation: #{request.body.read}"
     NewRelic::Agent.notice_error("CSP Violation: #{request.body.read}")
