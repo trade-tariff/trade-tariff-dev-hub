39a3324e50e4aa3c22418b797b89911f08000b14
William Fish <william.michael.fish@gmail.com>
2026-06-18 17:08:26 +0100

    chore: respect RuboCop exclusions in hooks
    Issue: No ticket/issue

--- flake.nix
+++ flake.nix
@@ -188,7 +188,7 @@
               enable = true;
               name = "rubocop";
               description = "Run RuboCop through Bundler";
-              entry = "bundle exec rubocop --autocorrect";
+              entry = "bundle exec rubocop --autocorrect --force-exclusion";
               files = "\\.(rb|rake)$|^(Gemfile|Rakefile|config\\.ru)$";
               stages = [ "pre-commit" ];
             };
